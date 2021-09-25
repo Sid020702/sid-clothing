4e36db177dfa11a2b15b61d5a5d5147db921f163
Sidharth <sidharth706@gmail.com>
2021-09-25 13:47:18 +0530

    Upated shop data with new data and creted cart.utils.js to group items in cart reducer on adding it multiple times

--- src/components/preview-collection/preview-collection.component.jsx
+++ src/components/preview-collection/preview-collection.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './preview-collection.styles.scss';
+import CollectionItem from '../collection-item/collection-item.components';
 
 
 const CollectionPreview = ({ title, items }) => (
@@ -8,7 +9,7 @@ const CollectionPreview = ({ title, items }) => (
         <div className='preview'>
             {
                 items.filter((item, idx) => idx < 4).map((item) => (
-                    <div key={item.id}>{item.name}</div>
+                    <CollectionItem key={item.id} item={item} />
                 ))
             }
         </div>
