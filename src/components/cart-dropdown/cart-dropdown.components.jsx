950ef33f2e8b94fbc6ecc9c094f3c519d16176eb
Sidharth <sidharth706@gmail.com>
2021-10-01 13:24:11 +0530

    Imported the toggleCartHidden cart action into dropdown component to toggle dropdown hidden on clicking checkout

--- src/components/cart-dropdown/cart-dropdown.components.jsx
+++ src/components/cart-dropdown/cart-dropdown.components.jsx
@@ -6,8 +6,10 @@ import { connect } from "react-redux";
 import { selectCartItems } from "../../redux/cart/cart.selectors ";
 import { createStructuredSelector } from "reselect";
 import { withRouter } from "react-router";
+import { toggleCartHidden } from '../../redux/cart/cart.actions'
 
-const CartDropdown = ({ cartItems, history }) => (
+const CartDropdown = ({ cartItems, history, dispatch }) =>
+(
     <div className='cart-dropdown'>
         <div className='cart-items'>
             {
@@ -18,7 +20,10 @@ const CartDropdown = ({ cartItems, history }) => (
                     :
                     (<span className='empty-message'>Your cart is empty.</span>)
             }
-            <CustomButton onClick={() => history.push('/checkout')}>GO TO CHECKOUT</CustomButton>
+            <CustomButton onClick={() => {
+                history.push('/checkout')
+                dispatch(toggleCartHidden())
+            }}>GO TO CHECKOUT</CustomButton>
         </div>
     </div>
 )
